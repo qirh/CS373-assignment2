c5d4e59543cc56b9cd43fc598a502ce33c54b0ed
saleh alghusson <almto3@hotmail.com>
2016-06-24 21:43:04 -0500

    submission

--- Netflix.py
+++ Netflix.py
@@ -9,7 +9,7 @@ from statistics import mean
 from numpy import mean, sqrt, square, subtract
 from urllib.request import urlopen
 
-#caches
+# caches
 
 #{int:dict{int:int} }: a dictionary cache, where the movie id was the key, and a dictionary was the value (that dictionary has customer_id's as keys, and the rating they gave as values
 cache_answers = {}
@@ -23,18 +23,18 @@ cache_customer_avg_location = "amm6364-averageCustomerRating.p"
 cache_movie_avg = {}
 cache_movie_avg_location = "amm6364-averageMovieRating.p"
 
-#constants defined by specs
+# constants defined by specs
 NUM_CUSTOMERS = 480189
 
 MIN_CUSTOMER_ID = 1
 MAX_CUSTOMER_ID = 2649429
-MIN_RATING_DATE = datetime.date(1998,10,1)
-MAX_RATING_DATE = datetime.date(2005,12,31)
+MIN_RATING_DATE = datetime.date(1998, 10, 1)
+MAX_RATING_DATE = datetime.date(2005, 12, 31)
 
 MIN_MOVIE_ID = 1
 MAX_MOVIE_ID = 17770
-MIN_MOVIE_DATE = datetime.date(1890,1,1)
-MAX_MOVIE_DATE = datetime.date(2005,12,31)
+MIN_MOVIE_DATE = datetime.date(1890, 1, 1)
+MAX_MOVIE_DATE = datetime.date(2005, 12, 31)
 
 
 # ------------
@@ -42,11 +42,11 @@ MAX_MOVIE_DATE = datetime.date(2005,12,31)
 # ------------
 
 def netflix_read(reader):
-	""" #pragma: no cover
-	s is a string representing a line
-	reads s
-	"""
-	return reader.readline().strip()
+    """ #pragma: no cover
+    s is a string representing a line
+    reads s
+    """
+    return reader.readline().strip()
 
 
 # -------------
@@ -54,12 +54,12 @@ def netflix_read(reader):
 # -------------
 
 def netflix_print(writer, n):
-	""" #pragma: no cover
-	w a writer
-	n is a number which is either the movie or customer id
-	prints n to w
-	"""
-	writer.write(str(n) + "\n")
+    """ #pragma: no cover
+    w a writer
+    n is a number which is either the movie or customer id
+    prints n to w
+    """
+    writer.write(str(n) + "\n")
 
 
 # --------------
@@ -67,10 +67,10 @@ def netflix_print(writer, n):
 # --------------
 
 def netflix_actual(movie_id, customer_id):
-	""" #pragma: no cover
-	"""
+    """ #pragma: no cover
+    """
 
-	return cache_answers[movie_id][customer_id]
+    return cache_answers[movie_id][customer_id]
 
 
 # ---------------
@@ -78,87 +78,93 @@ def netflix_actual(movie_id, customer_id):
 # ---------------
 
 def netflix_predict(movie_id, customer_id):
-	""" #pragma: no cover
-	"""
-	customer_avg = cache_customer_avg[customer_id]
-	customer_avg = cache_movie_avg[movie_id]
-	return 0
+    """ #pragma: no cover
+    """
+    customer_avg = cache_customer_avg[customer_id]
+    customer_avg = cache_movie_avg[movie_id]
+    return 0
 
 # ------------------
 # netflix_load_cache
 # ------------------
+
+
 def netflix_load_cache(cache_name):
-	""" #pragma: no cover
-	return a dict that represents the cache
-	"""
-	filepath = "/u/downing/public_html/netflix-caches/" + cache_name
+    """ #pragma: no cover
+    return a dict that represents the cache
+    """
+    filepath = "/u/downing/public_html/netflix-caches/" + cache_name
 
-	if os.path.isfile(filepath):
-		print("in system")
-		f = open(filepath, 'rb')
-		cache = pickle.load(f)
-		f.close() #always remember to close the file
+    if os.path.isfile(filepath):
+        print("in system")
+        f = open(filepath, 'rb')
+        cache = pickle.load(f)
+        f.close()  # always remember to close the file
 
-	else:
-		print("outside")
-		cache_url = "http://www.cs.utexas.edu/users/downing/netflix-caches/" + cache_name
-		cache_read_from_url = urlopen(cache_url).read()
-		cache = pickle.loads(cache_read_from_url)
-	return cache
+    else:
+        print("outside")
+        cache_url = "http://www.cs.utexas.edu/users/downing/netflix-caches/" + \
+            cache_name
+        cache_read_from_url = urlopen(cache_url).read()
+        cache = pickle.loads(cache_read_from_url)
+    return cache
 
 # ------------
 # netflix_rmse
 # ------------
+
+
 def netflix_rmse(squ_diff, num):
-	""" #pragma: no cover
-	sum is a square root sum
-	num is the number of elements
-	calculates the root mean square error
-	"""
-	return sqrt( mean( pow(subtract(squ_diff, num), 2)))
+    """ #pragma: no cover
+    sum is a square root sum
+    num is the number of elements
+    calculates the root mean square error
+    """
+    return sqrt(mean(pow(subtract(squ_diff, num), 2)))
 
-def rmse_numpy (a, p) :
+
+def rmse_numpy(a, p):
     return sqrt(mean(square(subtract(a, p))))
-
 
 
 # -------------
 # netflix_solve
 # -------------
 def netflix_solve(reader, writer):
-	"""
-	r a reader
-	w a writer
-	"""
-	#loading caches
-	cache_answers = netflix_load_cache(cache_answers_location)
-	cache_customer_avg = netflix_load_cache(cache_customer_avg_location)
-	cache_movie_avg = netflix_load_cache(cache_movie_avg_location)
+    """
+    r a reader
+    w a writer
+    """
+    # loading caches
+    cache_answers = netflix_load_cache(cache_answers_location)
+    cache_customer_avg = netflix_load_cache(cache_customer_avg_location)
+    cache_movie_avg = netflix_load_cache(cache_movie_avg_location)
 
-	count = 0
-	squ_diff = 0
-	flag = True
+    count = 0
+    squ_diff = 0
+    flag = True
 
-	while (flag) :
+    while (flag):
 
-		line = netflix_read(reader)
+        line = netflix_read(reader)
 
-		print("line -->" + line)
+        print("line -->" + line)
 
-		if not line :
-			netflix_print(writer, "RMSE: " + str(netflix_rmse(squ_diff,count)))
-			flag = False
+        if not line:
+            netflix_print(
+                writer, "RMSE: " + str(netflix_rmse(squ_diff, count)))
+            flag = False
 
-		elif line[-1] != ":" : #customer id
+        elif line[-1] != ":":  # customer id
 
-			prediction = netflix_predict(cache_customer_avg[line],cache_movie_avg[currentMovieID])
-			actual = cache_answers[currentMovieID + "-" + line]
-			squ_diff += ((actual - prediction) ** 2)
-			count += 1
+            prediction = netflix_predict(
+                cache_customer_avg[line], cache_movie_avg[currentMovieID])
+            actual = cache_answers[currentMovieID + "-" + line]
+            squ_diff += ((actual - prediction) ** 2)
+            count += 1
 
-			netflix_print(writer, prediction)
-	  
-		else : #movie id
-			currentMovieID=line[:-1]
-			netflix_print(writer, line)
+            netflix_print(writer, prediction)
 
+        else:  # movie id
+            currentMovieID = line[:-1]
+            netflix_print(writer, line)
--- TestNetflix.py
+++ TestNetflix.py
@@ -16,95 +16,103 @@ from Netflix import cache_answers_location, cache_customer_avg_location, cache_m
 # -----------
 # TestNetflix
 # -----------
+
+
 class TestNetflix (TestCase):
 
-	# ----
-	# read
-	# ----
-	def test_read_1(self):
+    # ----
+    # read
+    # ----
+    def test_read_1(self):
 
-		s = StringIO("467:\n")
-		i = netflix_read(s)
-		self.assertEqual(i, "467:")
+        s = StringIO("467:\n")
+        i = netflix_read(s)
+        self.assertEqual(i, "467:")
 
-	def test_read_2(self):
-		s = StringIO("1109700,3,2001-07-12\n")
-		i = netflix_read(s)
-		self.assertEqual(i, "1109700,3,2001-07-12")
-	
-	def test_read_3(self):
-		s = StringIO("4320:\n")
-		i = netflix_read(s)
-		self.assertEqual(i, "4320:")
+    def test_read_2(self):
+        s = StringIO("1109700,3,2001-07-12\n")
+        i = netflix_read(s)
+        self.assertEqual(i, "1109700,3,2001-07-12")
 
-	def test_read_4(self):
-		s = StringIO("1687310,2005-09-30\n")
-		i = netflix_read(s)
-		self.assertEqual(i, "1687310,2005-09-30")
+    def test_read_3(self):
+        s = StringIO("4320:\n")
+        i = netflix_read(s)
+        self.assertEqual(i, "4320:")
 
-	def test_read_5(self):
-		s = StringIO("5867:\n")
-		i = netflix_read(s)
-		self.assertEqual(i, "5867:")
+    def test_read_4(self):
+        s = StringIO("1687310,2005-09-30\n")
+        i = netflix_read(s)
+        self.assertEqual(i, "1687310,2005-09-30")
 
-	def test_read_6(self):
-		s = StringIO("1286101,2005-03-11\n")
-		i = netflix_read(s)
-		self.assertEqual(i, "1286101,2005-03-11")
+    def test_read_5(self):
+        s = StringIO("5867:\n")
+        i = netflix_read(s)
+        self.assertEqual(i, "5867:")
 
-	# -----
-	# print
-	# -----
-	def test_print_1(self):
-		w = StringIO()
-		netflix_print(w, 2)
-		self.assertEqual(w.getvalue(), "2\n")
-	def test_print_2(self):
-		w = StringIO()
-		netflix_print(w, 4525)
-		self.assertEqual(w.getvalue(), "4525\n")
-	def test_print_3(self):
-		w = StringIO()
-		netflix_print(w, 3)
-		self.assertEqual(w.getvalue(), "3\n")
-	def test_print_4(self):
-		w = StringIO()
-		netflix_print(w, 2)
-		self.assertEqual(w.getvalue(), "2\n")
-	def test_print_5(self):
-		w = StringIO()
-		netflix_print(w, 235325523)
-		self.assertEqual(w.getvalue(), "235325523\n")
-	def test_print_6(self):
-		w = StringIO()
-		netflix_print(w, 124214)
-		self.assertEqual(w.getvalue(), "124214\n")
-	def test_print_7(self):
-		w = StringIO()
-		netflix_print(w, 276578)
-		self.assertEqual(w.getvalue(), "276578\n")
+    def test_read_6(self):
+        s = StringIO("1286101,2005-03-11\n")
+        i = netflix_read(s)
+        self.assertEqual(i, "1286101,2005-03-11")
 
-	# ----
-	# rmse
-	# ----
-	def test_rmse_1(self):
-		n1 = 3
-		n2 = 4
-		squ_diff = ((n1 - n2) ** 2)
-		result = netflix_rmse(squ_diff, 2)
+    # -----
+    # print
+    # -----
+    def test_print_1(self):
+        w = StringIO()
+        netflix_print(w, 2)
+        self.assertEqual(w.getvalue(), "2\n")
 
-		print(str(squ_diff) + " -- " + str(result))
+    def test_print_2(self):
+        w = StringIO()
+        netflix_print(w, 4525)
+        self.assertEqual(w.getvalue(), "4525\n")
 
-		self.assertEqual(result, 2)
+    def test_print_3(self):
+        w = StringIO()
+        netflix_print(w, 3)
+        self.assertEqual(w.getvalue(), "3\n")
 
-	def test_rmse_2(self):
-		n1 = 3
-		n2 = 4
-		result = rmse_numpy(n1, n2)
+    def test_print_4(self):
+        w = StringIO()
+        netflix_print(w, 2)
+        self.assertEqual(w.getvalue(), "2\n")
 
-		print(str(result) + " -- " + str(result))
+    def test_print_5(self):
+        w = StringIO()
+        netflix_print(w, 235325523)
+        self.assertEqual(w.getvalue(), "235325523\n")
 
-		self.assertEqual(result, 2)
+    def test_print_6(self):
+        w = StringIO()
+        netflix_print(w, 124214)
+        self.assertEqual(w.getvalue(), "124214\n")
+
+    def test_print_7(self):
+        w = StringIO()
+        netflix_print(w, 276578)
+        self.assertEqual(w.getvalue(), "276578\n")
+
+    # ----
+    # rmse
+    # ----
+    def test_rmse_1(self):
+        n1 = 3
+        n2 = 4
+        squ_diff = ((n1 - n2) ** 2)
+        result = netflix_rmse(squ_diff, 2)
+
+        print(str(squ_diff) + " -- " + str(result))
+
+        self.assertEqual(result, 2)
+
+    def test_rmse_2(self):
+        n1 = 3
+        n2 = 4
+        result = rmse_numpy(n1, n2)
+
+        print(str(result) + " -- " + str(result))
+
+        self.assertEqual(result, 2)
 
 """
 	def test_load_cache_1(self):
@@ -131,7 +139,7 @@ class TestNetflix (TestCase):
 		self.assertEqual( cache[2046], {2403193, 3})
 		self.assertEqual( cache[2049], {2048459, 3})
 		self.assertEqual( cache[2049], {2220019, 3})
-"""		
+"""
 
 
 # ----
@@ -139,5 +147,4 @@ class TestNetflix (TestCase):
 # ----
 
 if __name__ == "__main__":
-	main()
-
+    main()
